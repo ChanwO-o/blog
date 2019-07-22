9d16b7a1f940b5dce1ff5a714768fd22c4aab7d6
Chan Woo Park <14cpark@gmail.com>
2019-07-22 17:42:41 +0000

    move app.component files to sub folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component/app.component';
 
 @NgModule({
   declarations: [ // register component to angular app
