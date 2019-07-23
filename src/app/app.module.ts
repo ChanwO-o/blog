118ead583262c699ee4217f1c4f62af8e8b02776
Chan Woo Park <14cpark@gmail.com>
2019-07-23 07:42:52 +0000

    install angular material designs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 // describes features of the app, split by modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // must import without file extension
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component/app.component';
-import { BlogpostViewComponent } from './blogposts/blogpost-view/blogpost-view.component'; // must import without file extension
+import { BlogpostViewComponent } from './blogposts/blogpost-view/blogpost-view.component';
 
 @NgModule({
   declarations: [ // register component to angular app
@@ -13,7 +14,8 @@ import { BlogpostViewComponent } from './blogposts/blogpost-view/blogpost-view.c
   ],
   imports: [ // other imported modules
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent] // tells angular to search the current running page for this component by its selector (<app-root>)
