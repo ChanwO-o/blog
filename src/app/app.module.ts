52b1b15e525a7ef63281c23935b2a62c6372351c
Chan Woo Park <14cpark@gmail.com>
2019-07-26 08:58:23 +0000

    install angular flex for responsive layouts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 // describes features of the app, split by modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // must import without file extension
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component/app.component';
-import { BlogpostViewComponent } from './blogposts/blogpost-view/blogpost-view.component';
+import { BlogpostViewComponent } from './blogposts/blogpost-view/blogpost-view.component'; // must import without file extension
 
 @NgModule({
   declarations: [ // register component to angular app
