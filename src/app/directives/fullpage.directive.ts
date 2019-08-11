8e179fbe00fc32d107636ee5b4a9132e5ffcb3aa
Chan Woo Park <14cpark@gmail.com>
2019-08-11 17:41:16 +0000

    implement resize()

--- src/app/directives/fullpage.directive.ts
+++ src/app/directives/fullpage.directive.ts
@@ -1,10 +1,37 @@
 import { Directive } from '@angular/core';
+import { ElementRef } from '@angular/core';
 
 @Directive({
-  selector: '[appFullpage]'
+	selector: '[appFullpage]'
 })
+
 export class FullpageDirective {
 
-  constructor() { }
+	constructor(private element: ElementRef) {}
 
+
+	resize() {
+		// get original dimensions
+		let bgWidth = this.element.nativeElement.width;
+		let bgHeight = this.element.nativeElement.height;
+
+		// get window dimensions
+		let winWidth = window.innerWidth;
+		let winHeight = window.innerHeight;
+
+		// calculate ratio
+		let widthRatio = winWidth / bgWidth;
+		let heightRatio = winHeight / bgHeight;
+
+		let widthDiff = heightRatio * bgWidth;
+		let heightDiff = widthRatio * bgHeight;
+
+		if (heightDiff > winHeight) {
+			this.element.nativeElement.width = winWidth;
+			this.element.nativeElement.height = heightDiff;
+		} else {
+			this.element.nativeElement.width = widthDiff;
+			this.element.nativeElement.height = winHeight;
+		}
+	}
 }
