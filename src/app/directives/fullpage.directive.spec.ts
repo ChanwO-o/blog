6b384b70e259223758443c514862cd4e8d1732d1
Chan Woo Park <14cpark@gmail.com>
2019-08-11 18:23:14 +0000

    pass elementRefMock into constructor

--- src/app/directives/fullpage.directive.spec.ts
+++ src/app/directives/fullpage.directive.spec.ts
@@ -1,8 +1,12 @@
 import { FullpageDirective } from './fullpage.directive';
 
+const elementRefMock = {
+	nativeElement: {}
+}
+
 describe('FullpageDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FullpageDirective();
-    expect(directive).toBeTruthy();
-  });
+	it('should create an instance', () => {
+		const directive = new FullpageDirective(elementRefMock);
+		expect(directive).toBeTruthy();
+	});
 });
