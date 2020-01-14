806d9dcfbe0cce039aa6de67a20010dc6366aa44
Chan Woo Park <14cpark@gmail.com>
2020-01-14 15:40:30 +0000

    fix blog post schema

--- backend/database/models/blogpost.js
+++ backend/database/models/blogpost.js
@@ -1,4 +1,21 @@
 const mongoose = require('mongoose');
+
+const CommentSchema = new mongoose.Schema({
+	author: {
+		type: String,
+		trim: true,
+		default: 'Anonymous'
+	},
+	timestamp: {
+		type: Date,
+		default: Date.now
+	},
+	comment: {
+		type: String,
+		default: 'Empty comment'
+	}
+});
+
 const BlogPostSchema = new mongoose.Schema({
 	title: {
 		type: String,
@@ -8,18 +25,23 @@ const BlogPostSchema = new mongoose.Schema({
 	author: {
 		type: String,
 		trim: true,
-		default: 'No author'
+		default: 'Anonymous'
 	},
 	timestamp: {
 		type: Date,
 		default: Date.now
+	},
+	tags: {
+		type: [String],
+		default: []
 	},
 	published: { // hide post if not published
 		type: Boolean,
 		default: true
 	},
 	body: {
-		type: String // html string
+		type: String, // html string
+		default: '<p>blog post body</p>'
 	},
 	upvotes: {
 		type: Number,
@@ -30,10 +52,11 @@ const BlogPostSchema = new mongoose.Schema({
 		default: 0
 	},
 	comments: {
-		type: Array,
+		type: [CommentSchema],
 		default: []
 	}
 });
+
 const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
 
 module.exports = BlogPost;
