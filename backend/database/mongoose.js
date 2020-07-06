91145df5f653ca6143629be15cd4a7469f4e39d4
Chan Woo Park <14cpark@gmail.com>
2020-07-06 03:26:43 -0700

    containerize mongo database and express api with Docker

--- backend/database/mongoose.js
+++ backend/database/mongoose.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; // handle asynchronous code
 
-mongoose.connect('mongodb://127.0.0.1:27017/blog', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+mongoose.connect('mongodb://database:27017/blog', // 'database': name of mongodb docker container
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false
+	})
 	.then(() => console.log("Database connected"))
 	.catch((error) => console.log(error));
 
